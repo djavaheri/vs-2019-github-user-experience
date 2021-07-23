d0dabc77837d914bd37f9aade20cbd20f346470f
Mohammad Djavaheri <DjavMoha@do-dat.org>
2021-07-23 14:21:22 +0200

    Hello first change

--- vs-2019-github-user-experience/Startup.cs
+++ vs-2019-github-user-experience/Startup.cs
@@ -51,7 +51,7 @@ namespace vs_2019_github_user_experience
             app.UseEndpoints(endpoints =>
             {
                 //endpoints.MapRazorPages();
-                endpoints.MapGet("/", async context => { await context.Response.WriteAsync("Hello world"); }
+                endpoints.MapGet("/", async context => { await context.Response.WriteAsync("Hello first change!"); }
                 );
             });
         }
